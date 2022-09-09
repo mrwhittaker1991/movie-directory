f8c234efe79a918c93a2751d7aa72e23a6978277
MichaelWhittaker21 <zalvazar@DESKTOP-7N2GVRP.localdomain>
2022-09-09 18:58:17 +0200

    progress

--- web/modules/custom/movie_directory/src/Form/MovieAPI.php
+++ web/modules/custom/movie_directory/src/Form/MovieAPI.php
@@ -6,12 +6,15 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 Class MovieAPI extends FormBase {
+
+    const MOVIE_API_CONFIG_PAGE = 'movie_api_config_page:values';
     
     public function getFormId() {
-        return 'Movie_api_config_page';
+        return 'movie_api_config_page';
     }
 
     public function buildForm(array $form, FormStateInterface $form_state) {
+        $values = \Drupal::state()->get(self::MOVIE_API_CONFIG_PAGE);
         $form = [];
 
         $form['api_base_url'] = [
@@ -19,7 +22,7 @@ Class MovieAPI extends FormBase {
             '#title' => t('API base url'),
             '#description' => $this->t('This is the API Base URL'),
             '#required' => TRUE,
-            '#default_value' => '',
+            '#default_value' => $values['api_base_url'],
         ];
 
         $form['api_key'] = [
@@ -27,14 +30,26 @@ Class MovieAPI extends FormBase {
             '#title' => $this->t('API key (v3 auth)'),
             '#description' => $this->t('This is the api key that will be used to access the API.'),
             '#required' => TRUE,
-            '#default_value' => '',
+            '#default_value' => $values['api_key'],
         ];
 
+        $form['actions']['#type']  = 'actions';
+        $form['actions']['submit'] = [
+            '#type' => 'submit',
+            '#value' => $this->t('Save'),
+            '#button_type' => 'primary'
+        ];
+        
         return $form;
     }
 
     public function submitForm(array &$form, FormStateInterface $form_state) {
-    
+        $submitted_values = $form_state->cleanValues()->getValues();
+
+        \Drupal::state()->set(self::MOVIE_API_CONFIG_PAGE, $submitted_values);
+
+        $messenger = \Drupal::service('messenger');
+        $messenger->addMessage($this->t('Your new configuration has been saved successfully.'));
     }
 }
 
